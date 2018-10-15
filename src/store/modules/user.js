419139a71d6fc06d37e7bfbac65fe9f6b006c703
jlylb <jlylb@126.com>
2018-10-15 09:04:46 +0800

    delete getuserinfo token

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -94,7 +94,7 @@ const user = {
     // 获取用户信息
     GetUserInfo({ commit, state }) {
       return new Promise((resolve, reject) => {
-        getUserInfo(state.token).then(response => {
+        getUserInfo().then(response => {
           console.log(response)
           if (!response.data) { // 由于mockjs 不支持自定义状态码只能这样hack
             reject('error')
