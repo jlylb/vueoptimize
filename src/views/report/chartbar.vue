deab5ad91438b866a8512b4692f29bf691db3136
jlylb <jlylb@126.com>
2018-12-11 16:37:39 +0800

    update history num type integer

--- src/views/report/chartbar.vue
+++ src/views/report/chartbar.vue
@@ -1,11 +1,15 @@
 <template>
-  <ve-histogram :after-config='afterConfig' height='100vh' :events='chartEvents' :extend='echartsData'></ve-histogram>
+  <ve-histogram
+    :after-config="afterConfig"
+    height="100vh"
+    :events="chartEvents"
+    :extend="echartsData"
+  ></ve-histogram>
 </template>
 
 <script>
-
 export default {
-  components: {  },
+  components: {},
   data() {
     return {
       echartsData: {},
@@ -13,193 +17,200 @@ export default {
       dataLevel: {},
       chartEvents: {},
       levelTxt: {
-        1: '一级告警',
-        2: '二级告警',
-        3: '三级告警',
-        4: '四级告警',
+        1: "一级告警",
+        2: "二级告警",
+        3: "三级告警",
+        4: "四级告警"
       }
-    }
+    };
   },
   props: {
     title: {
       type: [String, Number],
-      default: ''
+      default: ""
     },
     warn: {
       type: Array,
       default() {
-        return []
+        return [];
       }
     },
     level: {
       type: Array,
       default() {
-        return [
-
-        ]
+        return [];
       }
-    },
+    }
   },
   watch: {
     warn: {
       handler(newval) {
-        console.log(newval, 'warn ..............')
-        this.dataLevel = this.formatWarnLevel(newval)
-        this.echartsData = this.getData()
+        console.log(newval, "warn ..............");
+        this.dataLevel = this.formatWarnLevel(newval);
+        this.echartsData = this.getData();
       },
       immediate: true
     }
   },
-  mounted(){
-      this.chartEvents = {
-        click: (params) => {
-          this.barClick(params)
-        }
-    }
-   // this.echartsData = this.getData()
+  mounted() {
+    this.chartEvents = {
+      click: params => {
+        this.barClick(params);
+      }
+    };
+    // this.echartsData = this.getData()
   },
   methods: {
     afterConfig(options) {
       // console.log(this.getData(), 'after config.......1111111111111')
-     return this.echartsData
+      return this.echartsData;
     },
     barClick(params) {
-      const options = this.echartsData
-      const barData = params.data
+      const options = this.echartsData;
+      const barData = params.data;
       console.log(params);
-      if(params.componentSubType==='bar') {
-          let { pdi_index: pdiIndex } = barData
-          options.title[1].text = '设备: '+ pdiIndex + ' 告警等级占比'
-          options.title[1].subtext = '总计 ' + params.value
-          let pdiItem = this.formatKeys()[pdiIndex]
-        let pdiLevel = this.formatWarnLevel(pdiItem)
-          console.log(pdiItem, pdiLevel);
-          options.series[2]['data'] = Object.values(pdiLevel).map(function (item) {
-              return {
-                  name: item.name,
-                  value: item.num,
-                  pdi_index: pdiIndex,
-                  lvl: item.lvl
-              }
-          })
-            console.log(options);
-          this.echartsData = options
+      if (params.componentSubType === "bar") {
+        let { pdi_index: pdiIndex } = barData;
+        options.title[1].text = "设备: " + pdiIndex + " 告警等级占比";
+        options.title[1].subtext = "总计 " + params.value;
+        let pdiItem = this.formatKeys()[pdiIndex];
+        let pdiLevel = this.formatWarnLevel(pdiItem);
+        console.log(pdiItem, pdiLevel);
+        options.series[2]["data"] = Object.values(pdiLevel).map(function(item) {
+          return {
+            name: item.name,
+            value: +item.num,
+            pdi_index: pdiIndex,
+            lvl: item.lvl
+          };
+        });
+        console.log(options);
+        this.echartsData = options;
       }
-      if(params.componentSubType==='pie') {
-        let { pdi_index, lvl } = barData
+      if (params.componentSubType === "pie") {
+        let { pdi_index, lvl } = barData;
 
-        this.$router.push({name: 'api.report.index', params: {lvl: lvl, pdi_index}})
+        this.$router.push({
+          name: "api.report.index",
+          params: { lvl: lvl, pdi_index }
+        });
       }
     },
     getMax() {
-      return Math.max(...Object.values(this.getNum()))
+      return Math.max(...Object.values(this.getNum()));
     },
     getTotal() {
-      let nums = this.getNum()
+      let nums = this.getNum();
       return Object.keys(nums).reduce((all, item) => {
-        return all + nums[item]
-      }, 0)
+        return all + nums[item];
+      }, 0);
     },
     getNum() {
-      let nums = {}
-      for(const keyIndex in this.warn) {
-        let curKey = this.warn[keyIndex].pdi_index
-        let curVal = this.warn[keyIndex].value
-        if(!nums[curKey]) {
-          nums[curKey] = curVal
-        }else{
-          nums[curKey] += curVal
+      let nums = {};
+      for (const keyIndex in this.warn) {
+        let curKey = this.warn[keyIndex].pdi_index;
+        let curVal = this.warn[keyIndex].value;
+        if (!nums[curKey]) {
+          nums[curKey] = +curVal;
+        } else {
+          nums[curKey] += +curVal;
         }
       }
-      return nums
+      return nums;
     },
     formatWarn() {
-      let nums = {}
-      for(const keyIndex in this.warn) {
-        let curKey = this.warn[keyIndex].pdi_index
-        let curVal = this.warn[keyIndex].value
-        let lvl = this.warn[keyIndex].wd_level0
+      let nums = {};
+      for (const keyIndex in this.warn) {
+        let curKey = this.warn[keyIndex].pdi_index;
+        let curVal = this.warn[keyIndex].value;
+        let lvl = this.warn[keyIndex].wd_level0;
 
-        if(!nums[curKey]) {
-          nums[curKey] = { value: curVal, pdi_index: curKey, name: this.warn[keyIndex].name}
-        }else{
-          nums[curKey]['vaule'] += curVal
+        if (!nums[curKey]) {
+          nums[curKey] = {
+            value: curVal,
+            pdi_index: curKey,
+            name: this.warn[keyIndex].name
+          };
+        } else {
+          nums[curKey]["vaule"] += curVal;
         }
       }
-      return nums
+      return nums;
     },
     formatWarnLevel(data) {
-      let lvls = {}
-      for(const keyIndex in data) {
-        let curVal = data[keyIndex].value
-        let lvl = data[keyIndex].wd_level0
+      let lvls = {};
+      for (const keyIndex in data) {
+        let curVal = +data[keyIndex].value;
+        let lvl = data[keyIndex].wd_level0;
 
-        if(!lvls[lvl]) {
-          lvls[lvl] = { num: curVal, lvl: lvl, name: this.levelTxt[lvl] }
-        }else{
-          lvls[lvl]['num'] += curVal
+        if (!lvls[lvl]) {
+          lvls[lvl] = { num: curVal, lvl: lvl, name: this.levelTxt[lvl] };
+        } else {
+          lvls[lvl]["num"] += curVal;
         }
       }
-      return lvls
+      return lvls;
     },
     getKeys() {
-      let keys = []
-      for(const keyIndex in this.warn) {
-        let curKey = this.warn[keyIndex].pdi_index
-        if(keys.indexOf(curKey) < 0) {
-          keys.push(curKey)
-        }      
+      let keys = [];
+      for (const keyIndex in this.warn) {
+        let curKey = this.warn[keyIndex].pdi_index;
+        if (keys.indexOf(curKey) < 0) {
+          keys.push(curKey);
+        }
       }
-      return keys
+      return keys;
     },
     formatKeys() {
       let items = {};
-      for(const keyIndex in this.warn) {
-        let curKey = this.warn[keyIndex].pdi_index
-        if(!items[curKey]) {
-          items[curKey] = []
+      for (const keyIndex in this.warn) {
+        let curKey = this.warn[keyIndex].pdi_index;
+        if (!items[curKey]) {
+          items[curKey] = [];
         }
-        items[curKey].push(this.warn[keyIndex])
+        items[curKey].push(this.warn[keyIndex]);
       }
-      return items
+      return items;
     },
     getData() {
-
       const option = {
-       // backgroundColor: '#dfefbd',
-        backgroundColor: '#fff',
+        // backgroundColor: '#dfefbd',
+        backgroundColor: "#fff",
         legend: {
-            orient: 'vertical',
-            right: 10,
-            top: 20,
-            data: Object.values(this.levelTxt)
+          orient: "vertical",
+          right: 10,
+          top: 20,
+          data: Object.values(this.levelTxt)
         },
-    tooltip: {
-        show: true,
-        trigger: 'item',
-        // formatter: "{b}: {c} ({d}%)"
-    },
-    title: [{
-        text: '历史告警统计',
-        subtext: '总计 ' + this.getTotal(),
-        x: '25%',
-        textAlign: 'center'
-    }, {
-        text: '告警等级占比',
-        subtext: '总计 ' + this.getTotal(),
-        x: '75%',
-        textAlign: 'center'
-    }],
+        tooltip: {
+          show: true,
+          trigger: "item"
+          // formatter: "{b}: {c} ({d}%)"
+        },
+        title: [
+          {
+            text: "历史告警统计",
+            subtext: "总计 " + this.getTotal(),
+            x: "25%",
+            textAlign: "center"
+          },
+          {
+            text: "告警等级占比",
+            subtext: "总计 " + this.getTotal(),
+            x: "75%",
+            textAlign: "center"
+          }
+        ],
         xAxis: {
           type: "value",
-          max: this.getMax(),
+          max: this.getMax() + 20,
           splitLine: {
             show: false
           },
           axisTick: {
             show: false
           }
-         // data: times
+          // data: times
         },
         yAxis: {
           type: "category",
@@ -207,82 +218,82 @@ export default {
           axisLabel: {
             interval: 0,
             rotate: 30
-          },
+          }
         },
         grid: [
           {
-        top: 50,
-        width: '50%',
-        // bottom: '45%',
-        left: 10,
-        containLabel: true
-        }, {
-            top: '55%',
-            width: '50%',
+            top: 50,
+            width: "50%",
+            // bottom: '45%',
+            left: 10,
+            containLabel: true
+          },
+          {
+            top: "55%",
+            width: "50%",
             bottom: 0,
             left: 10,
             containLabel: true
-        }
+          }
         ],
         series: [
           {
-              type: 'bar',
-              stack: 'chart',
-              // barWidth:20,
-              z: 3,
-              label: {
-                  normal: {
-                      position: 'right',
-                      show: true
-                  }
-              },
-              data: Object.values(this.formatWarn())
-            }, 
-            {
-              type: 'bar',
-              stack: 'chart',
-              silent: true,
-              itemStyle: {
-                  normal: {
-                      color: '#eee'
-                  }
-              },
-              data: Object.values(this.formatWarn()).map((item) => {
-                  return this.getMax() - item.value;
-              })
+            type: "bar",
+            stack: "chart",
+            // barWidth:20,
+            z: 3,
+            label: {
+              normal: {
+                position: "right",
+                show: true
+              }
             },
-            {
-              type: 'pie',
-              radius: ['35%', '50%'],
-              color: ['rgba(194, 53, 49,0.9)', '#ff7e40', '#e6a23c', '#409eff'],
-              center: ['75%', '35%'],
-              tooltip: {
-                formatter: "{b}: {c} ({d}%)"
-              },
-              label: {
-                  normal: {
-                      // position: 'inner',
-                      formatter: '{b} {d}%',
+            data: Object.values(this.formatWarn())
+          },
+          {
+            type: "bar",
+            stack: "chart",
+            silent: true,
+            itemStyle: {
+              normal: {
+                color: "#eee"
+              }
+            },
+            data: Object.values(this.formatWarn()).map(item => {
+              return this.getMax() + 20 - item.value;
+            })
+          },
+          {
+            type: "pie",
+            radius: ["35%", "50%"],
+            color: ["rgba(194, 53, 49,0.9)", "#ff7e40", "#e6a23c", "#409eff"],
+            center: ["75%", "35%"],
+            tooltip: {
+              formatter: "{b}: {c} ({d}%)"
+            },
+            label: {
+              normal: {
+                // position: 'inner',
+                formatter: "{b} {d}%",
 
-                      textStyle: {
-                          // color: '#fff',
-                          fontWeight: 'bold',
-                          fontSize: 14
-                      }
-                  }
-              },
-              data: Object.values(this.dataLevel).map(function (item) {
-                  return {
-                      name: item.name,
-                      value: item.num,
-                      lvl: item.lvl
-                  }
-              })
+                textStyle: {
+                  // color: '#fff',
+                  fontWeight: "bold",
+                  fontSize: 14
+                }
+              }
+            },
+            data: Object.values(this.dataLevel).map(function(item) {
+              return {
+                name: item.name,
+                value: +item.num,
+                lvl: item.lvl
+              };
+            })
           }
-
         ]
-      }
-      return option
+      };
+      return option;
     }
   }
 };
--- src/views/report/index.vue
+++ src/views/report/index.vue
@@ -1,28 +1,26 @@
 <template>
-    <div class='table-layout'>
-        <table-list
-        :custom-columns='columns'
-        :form-columns='searchColumns'
-        :table-data='data'
-        :total='total'
-        :search-data='search'
-        :column-length='12'
-        :hide-action='true'
-        @list-data='getList'>
-
-         <template slot='wd_level0' slot-scope="{ data }">
-           <el-tag :class='warnClass(data.wd_level0)'> {{ data.wd_level0|warnTxt }} </el-tag>
-         </template>
-
-        </table-list>
-
-    </div>
+  <div class="table-layout">
+    <table-list
+      :custom-columns="columns"
+      :form-columns="searchColumns"
+      :table-data="data"
+      :total="total"
+      :search-data="search"
+      :column-length="12"
+      :hide-action="true"
+      @list-data="getList"
+    >
+      <template slot="wd_level0" slot-scope="{ data }">
+        <el-tag :class="warnClass(data.wd_level0)">{{ data.wd_level0|warnTxt }}</el-tag>
+      </template>
+    </table-list>
+  </div>
 </template>
 
 <script>
-import tableList from '../common/components/tableList'
-import { fetchList } from '@/api/report'
-import { warnLevel, warnClass } from '@/utils/config'
+import tableList from "../common/components/tableList";
+import { fetchList } from "@/api/report";
+import { warnLevel, warnClass } from "@/utils/config";
 
 export default {
   components: { tableList },
@@ -31,51 +29,50 @@ export default {
       data: [],
 
       searchColumns: [
-        { name: 'pdi_index', label: '设备编号', props: { clearable: true }},
-        { name: 'wd_index', label: '告警编号', props: { clearable: true }},
+        { name: "pdi_index", label: "设备编号", props: { clearable: true } },
+        { name: "wd_index", label: "告警编号", props: { clearable: true } },
         {
-          name: 'rs_updatetime',
-          label: '发送时间',
-          type: 'date'
+          name: "rs_updatetime",
+          label: "发送时间",
+          type: "date"
         }
       ],
       searchModel: {},
 
       columns: {
         rs_updatetime: {
-          label: '时间'
+          label: "时间"
         },
         hw_index: {
-          label: '索引'
+          label: "索引"
         },
         pdi_index: {
-          label: '设备编号'
+          label: "设备编号"
         },
         wd_index: {
-          label: '告警编号'
+          label: "告警编号"
         },
         pdi_code: {
-          label: '设备编码'
+          label: "设备编码"
         },
         Wc_classname: {
-          label: '告警名称'
+          label: "告警名称"
         },
         wd_warndesc: {
-          label: '告警描述'
+          label: "告警描述"
         },
         Rs_status: {
-          label: '告警状态'
+          label: "告警状态"
         },
         wd_level0: {
-          label: '告警等级'
+          label: "告警等级"
         },
         pdi_name: {
-          label: '设备名称'
+          label: "设备名称"
         },
         dt_typememo: {
-          label: '告警备注'
-        },
-
+          label: "告警备注"
+        }
       },
       total: 0,
       search: {
@@ -83,39 +80,36 @@ export default {
         pageSize: 10
       },
       detailData: {},
-      wdIndex: null,
-    }
+      wdIndex: null
+    };
   },
-  watch: {
-
-  },
+  watch: {},
   methods: {
     warnClass,
     getList(query) {
-      console.log(query)
-      fetchList(query || this.search).then((res) => {
-        this.data = res.data.data.data
-        this.total = res.data.data.total
-      }).catch((res) => {
-
-      })
+      console.log(query);
+      fetchList(query || this.search)
+        .then(res => {
+          this.data = res.data.data.data;
+          this.total = res.data.data.total;
+        })
+        .catch(res => {});
     },
     detail(item) {
-      this.wdIndex = item.wd_index
+      this.wdIndex = item.wd_index;
     }
-
   },
   filters: {
     warnTxt(val) {
-      return warnLevel()[val]['lvl']
+      return warnLevel()[val] ? warnLevel()[val]["lvl"] : "无";
     }
   },
   created() {
     this.search = Object.assign({}, this.search, this.$route.params);
-   // this.searchModel = { pdi_index: pdiIndex };
+    // this.searchModel = { pdi_index: pdiIndex };
     this.getList();
   }
-}
+};
 </script>
 <style lang="scss" scoped>
 .show-detail .el-form-item {
